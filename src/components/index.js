cdbe2be6b5ee4faf581518832abaeaf033d0af55
NehemiahLimo <nehemiahlimo02@gmail.com>
2018-09-25 20:42:55 +0300

    update on api-dataelements

---
+++ src/components/index.js
@@ -0,0 +1,20 @@
+import DataElements from './DataElements'
+import Datasets from './Datasets'
+import FacilityList from './FacilityList'
+import Indicators from './Indicators'
+
+import Navigator from './Navigator'
+import OrganisationUnits from './OrganisationUnits'
+import ExportLink from './ExportLink'
+import Elemente from './Elemente'
+import orgUni from './orgUni'
+export {
+  DataElements,
+  Datasets,
+  FacilityList,
+  Indicators,
+  Navigator,
+  OrganisationUnits,
+  Elemente,
+  orgUni
+}
